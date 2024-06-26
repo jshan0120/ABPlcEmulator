57d9261dfe97e1ea556430f96830df2baff859b5
jshan <jshan@cle.vision>
2024-06-26 11:18:47 +0900

    Initial Creation of Repository

---
+++ AbPlcEmulator.Models/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbPlcEmulator.Models
+{
+    public class Config
+    {
+        public string TagFilePath { get; set; }
+        public string ContainerName { get; set; }
+    }
+}
